ed0dea52214351dec9d533d356573eca9ee2a702
tranphat180603 <you@example.com>
2024-10-13 12:45:00 +0700

    t

--- r.py
+++ r.py
@@ -581,10 +581,10 @@ def main():
 
     # Extract problem cases
     if args.dataset_local_path: #handle local dataset (folder structured)
-        problem_cases = [extract_problem_cases_from_folder(args.dataset_local_path)]
+        problem_cases = extract_problem_cases_from_folder(args.dataset_local_path)
         num_workers = 1
         if args.local_ds_idx:
-            problem_batches = [problem_cases[args.local_ds_idx]]
+            problem_batches = [[problem_cases[args.local_ds_idx]]]
             print(f"Finding specific problem: {problem_batches['name']}")
         else:
             print("Processing all problems in the folder")
