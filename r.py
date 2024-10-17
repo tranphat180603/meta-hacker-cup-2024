8842d90e8d70c6e24f17dee24c3d7b978ad2a94b
tranphat180603 <111284337+tranphat180603@users.noreply.github.com>
2024-10-17 12:40:32 +0700

    Update r.py

--- r.py
+++ r.py
@@ -53,7 +53,7 @@ def load_model_and_tokenizer(model_name, adapter_path ,temperature=0.3):
 
 
 #Load model
-model_name = "Qwen/Qwen2.5-72B"
+model_name = "Qwen/Qwen2.5-Coder-7B-Instruct"
 lora_path = "./adapter/"
 model, tokenizer = load_model_and_tokenizer(model_name,lora_path,temperature=0.3)
 print(f"USING MODEL: {model_name}")
